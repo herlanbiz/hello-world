3841e33c32dab4d67d9549f9eb74a3d34ced1020
herlanbiz <32919993+herlanbiz@users.noreply.github.com>
2022-10-19 13:16:25 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,14 +1,4 @@
-const http = require('http');
-
-const hostname = '43.245.181.75';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
